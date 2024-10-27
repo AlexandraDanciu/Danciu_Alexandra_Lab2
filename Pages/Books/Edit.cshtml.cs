a064e7ccf9541437e7c1aada1a2f94308e3e8093
ALEXANDRA-IOANA DANCIU <alexandra.ioana.danciu@stud.ubbcluj.ro>
2024-10-27 21:23:10 +0200

    laborator3

--- Pages/Books/Edit.cshtml.cs
+++ Pages/Books/Edit.cshtml.cs
@@ -8,10 +8,12 @@ using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
 using Danciu_Alexandra_Lab2.Data;
 using Danciu_Alexandra_Lab2.Models;
+using Danciu_Alexandra_Lab2.Models;
+
 
 namespace Danciu_Alexandra_Lab2.Pages.Books
 {
-    public class EditModel : PageModel
+    public class EditModel : BookCategoriesPageModel
     {
         private readonly Danciu_Alexandra_Lab2.Data.Danciu_Alexandra_Lab2Context _context;
 
@@ -21,7 +23,7 @@ namespace Danciu_Alexandra_Lab2.Pages.Books
         }
 
         [BindProperty]
-        public Book Book { get; set; } = default!;
+        public Book Book { get; set; }
 
         public async Task<IActionResult> OnGetAsync(int? id)
         {
@@ -30,12 +32,20 @@ namespace Danciu_Alexandra_Lab2.Pages.Books
                 return NotFound();
             }
 
-            var book =  await _context.Book.FirstOrDefaultAsync(m => m.ID == id);
-            if (book == null)
+
+            Book = await _context.Book
+              .Include(b => b.Publisher)
+              .Include(b => b.Author)
+              .Include(b => b.BookCategories).ThenInclude(b => b.Category)
+              .AsNoTracking()
+              .FirstOrDefaultAsync(m => m.ID == id);
+
+            if (Book == null)
             {
                 return NotFound();
             }
-            Book = book;
+
+            PopulateAssignedCategoryData(_context, Book);
 
             var authorList = _context.Author.Select(x => new
             {
@@ -45,43 +55,45 @@ namespace Danciu_Alexandra_Lab2.Pages.Books
             );
             ViewData["PublisherID"] = new SelectList(_context.Set<Publisher>(), "ID", "PublisherName");
             ViewData["AuthorID"] = new SelectList(authorList, "ID", "FullName");
-           
+
             return Page();
         }
 
         // To protect from overposting attacks, enable the specific properties you want to bind to.
         // For more information, see https://aka.ms/RazorPagesCRUD.
-        public async Task<IActionResult> OnPostAsync()
+        public async Task<IActionResult> OnPostAsync(int? id, string[]selectedCategories)
         {
-            if (!ModelState.IsValid)
+            if (id == null)
             {
-                return Page();
+                return NotFound();
             }
 
-            _context.Attach(Book).State = EntityState.Modified;
-
-            try
+            var bookToUpdate = await _context.Book
+                .Include(b=>b.Author)
+               .Include(i => i.Publisher)
+               .Include(i => i.BookCategories)
+                   .ThenInclude(i => i.Category)
+               .FirstOrDefaultAsync(s => s.ID == id);
+            if (bookToUpdate == null)
             {
+                return NotFound();
+            }
+            if (await TryUpdateModelAsync<Book>(
+               bookToUpdate,
+               "Book",
+               i => i.Title, i => i.Author, i => i.AuthorID,
+                i => i.Price, i => i.PublishingDate, i => i.PublisherID))
+            {
+                UpdateBookCategories(_context, selectedCategories, bookToUpdate);
                 await _context.SaveChangesAsync();
+                return RedirectToPage("./Index");
             }
-            catch (DbUpdateConcurrencyException)
-            {
-                if (!BookExists(Book.ID))
-                {
-                    return NotFound();
-                }
-                else
-                {
-                    throw;
-                }
-            }
-
-            return RedirectToPage("./Index");
-        }
-
-        private bool BookExists(int id)
-        {
-            return _context.Book.Any(e => e.ID == id);
+            UpdateBookCategories(_context, selectedCategories, bookToUpdate);
+            PopulateAssignedCategoryData(_context, bookToUpdate);
+            return Page();
         }
     }
-}
+}          
+            
+            
+            
